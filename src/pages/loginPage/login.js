47c6c88313a2e73e1d4f158ee13b60ce4b5e27ea
joeTheK <35149747+joeTheK@users.noreply.github.com>
2020-02-04 20:16:53 -0600

    Installed and tested bootstrap!

--- src/pages/loginPage/login.js
+++ src/pages/loginPage/login.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import './login.css';
-
+import "./login.css";
 
 class Login extends Component {
-    
-    render() {
-        return (
-            <div className="App">
-                <h1>This Will be the login Page!</h1>
-                <small>Login Page</small>
-                <Link to="/home">{<button> Go On! </button>}</Link>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>This Will be the login Page!</h1>
+        <small>Login Page</small>
+        <div>
+          <Link to="/home">
+            {<button className="btn btn-info"> Go On! </button>}
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
