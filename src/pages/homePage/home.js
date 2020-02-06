d1ae84c2b94af74a1083c94d4d4604cdcabfc7c2
joeTheK <35149747+joeTheK@users.noreply.github.com>
2020-02-06 16:11:42 -0600

    asd

--- src/pages/homePage/home.js
+++ src/pages/homePage/home.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import "./home.css";
 
 class Home extends Component {
-
-    render(){
-        return (
-            <div className="App">
-                <h1>This Will be the Home Page!</h1>
-                <small>Home Page</small>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <h1>This Will be the Home Page!</h1>
+        <small>Home Page</small>
+      </div>
+    );
+  }
 }
 
 export default Home;
