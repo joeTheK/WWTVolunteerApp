9dc705c372b481d9253b13e521b05b26bbeb3d2d
cong3054 <53188856+cong3054@users.noreply.github.com>
2020-02-08 22:56:47 -0600

    login design changes

--- src/pages/loginPage/login.js
+++ src/pages/loginPage/login.js
@@ -1,79 +1,21 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import "./login.css";
+import 'font-awesome/css/font-awesome.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGoogle} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+library.add(faGoogle);
+
 
 class Login extends Component {
   render() {
     return (
-      <div className="LoginBody">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-              <div className="card card-signin my-5">
-                <div className="card-body">
-                  <h5 className="card-title text-center">Sign In</h5>
-                  <form className="form-signin">
-                    <div className="form-label-group">
-                      <input
-                        type="email"
-                        id="inputEmail"
-                        className="form-control"
-                        placeholder="Email address"
-                        required
-                        autofocus
-                      />
-                      <label for="inputEmail">Email address</label>
-                    </div>
-                    <div className="form-label-group">
-                      <input
-                        type="password"
-                        id="inputPassword"
-                        className="form-control"
-                        placeholder="Password"
-                        required
-                      />
-                      <label for="inputPassword">Password</label>
-                    </div>
-                    <div className="custom-control custom-checkbox mb-3">
-                      <input
-                        type="checkbox"
-                        className="custom-control-input"
-                        id="customCheck1"
-                      />
-                      <label
-                        className="custom-control-label"
-                        for="customCheck1"
-                      >
-                        Remember password
-                      </label>
-                    </div>
-                    <button
-                      className="btn btn-lg btn-primary btn-block text-uppercase"
-                      type="submit"
-                    >
-                      Sign in
-                    </button>
-                    <hr className="my-4" />
-                    <button
-                      className="btn btn-lg btn-google btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab fa-google mr-2"></i> Sign in with Google
-                    </button>
-                    <button
-                      className="btn btn-lg btn-facebook btn-block text-uppercase"
-                      type="submit"
-                    >
-                      <i className="fab fa-facebook-f mr-2"></i> Sign in with
-                      Facebook
-                    </button>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="App">
+      <div id="box">
+        <FontAwesomeIcon icon="Google"  />
+        <button id="google" className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="Google"></i>Sign in with Google</button>
       </div>
+    </div>
     );
   }
 }
