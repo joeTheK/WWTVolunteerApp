48a8a4837b4ea8be4592b0a2f1219a5b1381d40c
joeTheK <35149747+joeTheK@users.noreply.github.com>
2020-02-09 21:34:32 -0600

    Did Some NavBar Stuffs -Joe

--- src/pages/communityPage/community.js
+++ src/pages/communityPage/community.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./community.css";
+import Navbar from "../components/Navbar";
 
 class Community extends Component {
   render() {
     return (
       <div className="CommunityBody">
+        <Navbar />
         <h1>This Will be the Community Page!</h1>
         <small>Community Page</small>
       </div>
--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -3,13 +3,59 @@ import { Link } from "react-router-dom";
 import "./components.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+  //Using State for Nav Method https://www.bennettnotes.com/bootstrap-navbar-collapse-reactjs/
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
   render() {
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed
+      ? "collapse navbar-collapse"
+      : "collapse navbar-collapse show";
+    const classTwo = collapsed
+      ? "navbar-toggler navbar-toggler-right collapsed"
+      : "navbar-toggler navbar-toggler-right";
     return (
-      <div className="container">
-        <nav className="navbar fixed-top navbar-light bg-light">
-          <Link to="/home">{<p className="navbar-brand">Yeet</p>}</Link>
-        </nav>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/home">
+          Volunteer APP
+        </a>
+        <button
+          onClick={this.toggleNavbar}
+          className={`${classTwo}`}
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className={`${classOne}`} id="navbarResponsive">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/home">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/community">
+                Community
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
