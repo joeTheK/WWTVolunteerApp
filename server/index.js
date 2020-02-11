b183626ccfc38b52885945290fa73607cbc7be13
cpatrick67135 <42450486+cpatrick67135@users.noreply.github.com>
2020-02-10 19:01:18 -0800

    concurrently nodemon + server

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const env.process.NODE_ENV;
-const port = process.env.PORT || 5000;
+var db = require('./database')
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
@@ -11,5 +13,15 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.listen(PORT, () => {
-    console.log('server listening on ${PORT}...');
+    console.log('server listening on ' + PORT + "...");
+});
+
+db.query('SELECT NOW()', (err, res) => {
+    if (err.error){
+        return console.log(err.error);
+    }else{
+        console.log('PostgreSQL connected: ' + res[0].now);
+    }
 })
+
+module.exports = app;
