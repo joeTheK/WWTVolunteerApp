fcbe258aec4c849da0925417c4df0b4af5c65540
joeTheK <35149747+joeTheK@users.noreply.github.com>
2020-02-13 16:38:16 -0600

    Log Form ready for styling

--- src/pages/homePage/logHourForm.js
+++ src/pages/homePage/logHourForm.js
@@ -5,22 +5,63 @@ class LogHourForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      totalHours: 0
+      hours: 0,
+      siteName: "",
+      siteAddress: "",
+      coordEmail: ""
     };
   }
   mySubmitHandler = event => {
     event.preventDefault();
-    alert("You are submitting");
+    alert(
+      "You are submitting with " +
+        this.state.hours +
+        " hours " +
+        this.state.siteName +
+        " siteName " +
+        this.state.siteAddress +
+        " siteAddress " +
+        this.state.coordEmail +
+        " coordEmail"
+    );
     //export this.state.totalHours
   };
-  myChangeHandler = event => {
-    this.setState({ totalHours: event.target.value });
+  hourChangeHandler = event => {
+    this.setState({ hours: event.target.value });
+  };
+  nameChangeHandler = event => {
+    this.setState({ siteName: event.target.value });
+  };
+  addressChangeHandler = event => {
+    this.setState({ siteAddress: event.target.value });
+  };
+  emailChangeHandler = event => {
+    this.setState({ coordEmail: event.target.value });
   };
   render() {
     return (
       <form onSubmit={this.mySubmitHandler}>
         <h1>Hello {this.props.userName}</h1>
-        <input type="time" onChange={this.myChangeHandler} />
+        <input
+          type="number"
+          placeholder="Hours Served"
+          onChange={this.hourChangeHandler}
+        />
+        <input
+          type="text"
+          placeholder="Site Name"
+          onChange={this.nameChangeHandler}
+        ></input>
+        <input
+          type="text"
+          placeholder="Site Address"
+          onChange={this.addressChangeHandler}
+        ></input>
+        <input
+          type="email"
+          placeholder="Email of Coordinator"
+          onChange={this.emailChangeHandler}
+        ></input>
         <input type="submit" />
       </form>
     );
