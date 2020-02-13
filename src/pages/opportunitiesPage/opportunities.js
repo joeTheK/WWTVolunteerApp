fd4c19fcb54916db4eca6f036dde975e4d2a702b
Zaid Alhajeri <zaidalhajeri@saads-MBP.mshome.net>
2020-02-13 16:25:04 -0600

    Data Table to Oppurtunities page. run npm install tabletop

--- src/pages/opportunitiesPage/opportunities.js
+++ src/pages/opportunitiesPage/opportunities.js
@@ -1,14 +1,70 @@
 import React, { Component } from "react";
 import "./opportunities.css";
 import Navbar from "../components/Navbar";
-
+//
+import Tabletop from 'tabletop';
+//Dynamically pulls from google sheets: requires npm install tabletop
 class Opportunities extends Component {
+  //Data Table Stuff:
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount() {
+    Tabletop.init({
+      key: '1KePg5zYNccqfqvO5skrvSYPPEyzWpqrFq-2MlfxTQJ4',
+      //Key of the Google Sheets page
+      callback: googleData => {
+        this.setState({
+          data: googleData
+        })
+      },
+      simpleSheet: true
+    })
+  }
+  //^^ Data Table Stuff
+
   render() {
+    //Mess with Table CSS here:
+    var tableStyle = {
+      "border": "1px solid black",
+      borderright: '1px solid red',
+      paddingTop : '0px',
+      paddingBottom : '0px',
+      paddingLeft:'0px',
+      paddingRight:'10px',
+      fontSize: 'small',
+      padding: '0',
+      margin: '0'
+   };
+   const { data } = this.state
+//table stuff^^
     return (
       <div className="OpportunitiesBody">
         <Navbar />
-        <h1>This Will be the Opportunities Page!</h1>
-        <small>Opportunities Page</small>
+        { 
+            data.map(obj => {
+              // Table below pulls from google sheet
+              return (  
+                // obj.orgName pulls info from column header OrgName, 
+                //repeats for length of spreadsheet
+                <div key={obj.OrgName}>      
+                  <table style={tableStyle}> 
+                  <td>{obj.OrgName}</td>
+                  <td>{obj.Categories}</td>
+                  <td>{obj.DescriptionMission }</td>
+                  <td>{obj.VolunteerForm}</td>
+                  <td>{obj.DonationLink}</td>
+                  <td>{obj.Email}</td>
+                  <td>{obj.Phone}</td>
+                  <td>{obj.Address}</td>
+                  </table>
+                </div>
+              )
+            })
+          }
       </div>
     );
   }
