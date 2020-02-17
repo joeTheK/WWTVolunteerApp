dddcf7a8a014d58eb66b60b9a383b44c16c13a00
trishten <42450374+trishten@users.noreply.github.com>
2020-02-16 21:55:24 -0600

    updated change to the opportunities page

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/community" component={Community} />
         <Route exact path="/opportunities" component={Opportunities} />
+        <Route exact path="/opportunities/:op" component={Opportunities} />
         <Route exact path="/404" component={NotFound} />
         {/* <Route exact path="/api" component={Api} /> */}
         <Redirect to="/404" />
--- src/pages/opportunitiesPage/opportunities.js
+++ src/pages/opportunitiesPage/opportunities.js
@@ -1,70 +1,130 @@
 import React, { Component } from "react";
 import "./opportunities.css";
 import Navbar from "../components/Navbar";
+import { Card } from "semantic-ui-react";
 //
-import Tabletop from 'tabletop';
+import Tabletop from "tabletop";
 //Dynamically pulls from google sheets: requires npm install tabletop
 class Opportunities extends Component {
   //Data Table Stuff:
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    const { match } = this.props;
+    console.log(match);
+
     this.state = {
-      data: []
-    }
+      data: [],
+      op: match.params["op"]
+    };
   }
   componentDidMount() {
     Tabletop.init({
-      key: '1KePg5zYNccqfqvO5skrvSYPPEyzWpqrFq-2MlfxTQJ4',
+      key: "1KePg5zYNccqfqvO5skrvSYPPEyzWpqrFq-2MlfxTQJ4",
       //Key of the Google Sheets page
       callback: googleData => {
         this.setState({
           data: googleData
-        })
+        });
       },
       simpleSheet: true
-    })
+    });
   }
   //^^ Data Table Stuff
 
   render() {
     //Mess with Table CSS here:
     var tableStyle = {
-      "border": "1px solid black",
-      borderright: '1px solid red',
-      paddingTop : '0px',
-      paddingBottom : '0px',
-      paddingLeft:'0px',
-      paddingRight:'10px',
-      fontSize: 'small',
-      padding: '0',
-      margin: '0'
-   };
-   const { data } = this.state
-//table stuff^^
+      border: "1px solid black",
+      borderright: "1px solid red",
+      paddingTop: "0px",
+      paddingBottom: "0px",
+      paddingLeft: "0px",
+      paddingRight: "10px",
+      fontSize: "small",
+      padding: "0",
+      margin: "0"
+    };
+    const { data } = this.state;
+    //table stuff^^
+    if (this.state.op === "tstyle") {
+      console.log(data);
+      return (
+        <div className="OpportunitiesBody">
+          <Navbar />
+          <h1>What i want to see!!</h1>
+          {data.map(doc => {
+            // return (
+            //   <>
+            //     <div className="altLayout">
+            //       <h2>{doc.OrgName}</h2>
+            //       <div>
+            //         <b>Address</b> <i>{doc.Address}</i>
+            //       </div>
+            //     </div>
+            //   </>
+            // );
+            return (
+              <>
+                <Card>
+                  <Card.Content className="test">
+                    <Card.Header>{doc.OrgName}</Card.Header>
+                    <Card.Meta>{doc.Address}</Card.Meta>
+                    <a
+                      href={doc.DonationLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Donate Now
+                    </a>
+                    &nbsp;
+                    <a
+                      href={doc.VolunteerForm}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Apply now
+                    </a>
+                    <br />
+                    <a
+                      href={doc.Homepage}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Contact Us
+                    </a>
+                    <p>{doc.DescriptionMission}</p>
+                  </Card.Content>
+                </Card>
+              </>
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="OpportunitiesBody">
         <Navbar />
-        { 
-            data.map(obj => {
-              // Table below pulls from google sheet
-              return (  
-                // obj.orgName pulls info from column header OrgName, 
-                //repeats for length of spreadsheet
-                <div key={obj.OrgName}>      
-                  <table style={tableStyle}> 
-                  <td>{obj.OrgName}</td>
-                  <td>{obj.Categories}</td>
-                  <td>{obj.DescriptionMission }</td>
-                  <td>{obj.VolunteerForm}</td>
-                  <td>{obj.DonationLink}</td>
-                  <td>{obj.Email}</td>
-                  <td>{obj.Phone}</td>
-                  <td>{obj.Address}</td>
-                  </table>
-                </div>
-              )
-            })
-          }
+        {console.log("i hit this")}
+        {data.map(obj => {
+          // Table below pulls from google sheet
+          return (
+            // obj.orgName pulls info from column header OrgName,
+            //repeats for length of spreadsheet
+            <div key={obj.OrgName}>
+              <table style={tableStyle}>
+                <td>{obj.OrgName}</td>
+                <td>{obj.Categories}</td>
+                <td>{obj.DescriptionMission}</td>
+                <td>{obj.VolunteerForm}</td>
+                <td>{obj.DonationLink}</td>
+                <td>{obj.Email}</td>
+                <td>{obj.Phone}</td>
+                <td>{obj.Address}</td>
+              </table>
+            </div>
+          );
+        })}
       </div>
     );
   }
