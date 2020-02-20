37eeacc29d6fa12d01c8f1304d2098473e0e0446
cong3054 <53188856+cong3054@users.noreply.github.com>
2020-02-20 09:04:49 -0600

    We have a twitter feed!

--- src/pages/communityPage/community.js
+++ src/pages/communityPage/community.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./community.css";
 import Navbar from "../components/Navbar";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
 
 class Community extends Component {
   render() {
@@ -9,8 +10,14 @@ class Community extends Component {
         <div className="CommunityBody">
           <Navbar />
           <div className="mainContent">
-            <div className="feed">feed</div>
-
+            <div className="feed">
+              <h1>Feed</h1>
+              <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName="VolunP"
+              options={{height: 1000}}
+              />
+            </div>
             <div className="maps">maps</div>
 
             <div className="incent">incentives</div>
