91044abeb382d60828bb50b3ba7d131ed7e7e7b7
trishten <42450374+trishten@users.noreply.github.com>
2020-02-24 20:27:19 -0600

    map done. Have to create a filter now.

--- src/pages/testMap/map.js
+++ src/pages/testMap/map.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 import Navbar from "../components/Navbar";
 import "./map.css";
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+//import "leaflet/dist/leaflet.css";
 import * as siteData from "./map.json";
-import L from "leaflet";
+
+// let config = {};
+// config.params = {
+//   center: [40.655769,-73.938503],
+//   zoomControl: false,
+//   zoom: 13,
+//   maxZoom: 19,
+//   minZoom: 11,
+//   scrollwheel: false,
+//   legends: true,
+//   infoControl: false,
+//   attributionControl: true
+// };
+// config.tileLayer = {
+//   uri: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+//   params: {
+//     minZoom: 11,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+//     id: '',
+//     accessToken: ''
+//   }
+// };
 
 class Maps extends Component {
   render() {
     return (
-    
       <React.Fragment>
         <Navbar />
         <LeafletMap
@@ -23,19 +43,27 @@ class Maps extends Component {
           animate={true}
           easeLinearity={0.35}
         >
-          <TileLayer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          <Marker position={[38.65, -90.25]}>
+            <img
+              src={
+                "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
+              }
+              width="15"
+            ></img>
+          </Marker>
           {siteData.features.map(site => {
             console.log(site.geometry.coordinates);
             return (
               <Marker
                 key={site.properties.name}
                 position={[
-                  site.geometry.coordinates[0],
                   site.geometry.coordinates[1],
+                  site.geometry.coordinates[0]
                 ]}
-              />
+              ></Marker>
             );
           })}
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
         </LeafletMap>
       </React.Fragment>
     );
