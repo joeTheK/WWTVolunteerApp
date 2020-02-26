6ce515a066fd04f4b3e0f8ad9343a5f2904ca234
trishten <42450374+trishten@users.noreply.github.com>
2020-02-26 16:11:25 -0600

    fixed maps

--- src/pages/communityPage/community.js
+++ src/pages/communityPage/community.js
@@ -7,7 +7,7 @@ import trophy from "./trophy.png";
 import Tabletop from "tabletop";
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import * as siteData from "./map.json";
-import './map.css'
+import "./map.css";
 
 class Community extends Component {
   constructor() {
@@ -42,123 +42,52 @@ class Community extends Component {
             </div>
             <div className="maps">
               <LeafletMap
-          center={[38.65, -90.25]}
-          zoom={11}
-          attributionControl={true}
-          zoomControl={true}
-          doubleClickZoom={true}
-          scrollWheelZoom={false}
-          dragging={true}
-          animate={true}
-          easeLinearity={0.35}
-                        >
-                         
-      
-        
-      <React.Fragment>
-         
-      <LeafletMap
-       center={[38.65, -90.25]}
-        zoom={11}
-        attributionControl={true}
-          zoomControl={true}
-          doubleClickZoom={true}
-          scrollWheelZoom={false}
-            dragging={true}
-              animate={true}
-             easeLinearity={0.35}
-             >
-           <Marker position={[38.65, -90.25]}>
-              <img
-              src={
-                "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
-                }
-                 width="15"
-                ></img>
-              </Marker>
-               {siteData.features.map(site => {
-             console.log(site.geometry.coordinates);
-              return (
-            <Marker
-                 key={site.properties.amenity}
-                  position={[
-                  site.geometry.coordinates[1],
-                  site.geometry.coordinates[0]
-                   ]}
-                                            >
-                                                <Popup>
-                                                    <h8>{site.properties.name}</h8>
-                                                    <p>{site.properties.amenity}</p>
-                                                </Popup>
-                                            </Marker>
-                                        );
-                                    })}
-                                    class Maps extends Component {
-  
-      <React.Fragment>
-      
-        //<LeafletMap
-          center={[38.65, -90.25]}
-          zoom={11}
-          attributionControl={true}
-          zoomControl={true}
-          doubleClickZoom={true}
-         scrollWheelZoom={false}
-          dragging={true}
-          animate={true}
-          easeLinearity={0.35}
-        >
-         <Marker position={[38.65, -90.25]}>
-            <img
-              src={
-                "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
-              }
-              width="15"
-            ></img>
-</Marker>
-          {siteData.features.map(site => {
-            console.log(site.geometry.coordinates);
-           return (
-              <Marker
-                key={site.properties.amenity}
-                position={[
-                 site.geometry.coordinates[1],
-                  site.geometry.coordinates[0]
-                ]}
+                center={[38.65, -90.25]}
+                zoom={11}
+                attributionControl={true}
+                zoomControl={true}
+                doubleClickZoom={true}
+                scrollWheelZoom={false}
+                dragging={true}
+                animate={true}
+                easeLinearity={0.35}
               >
-                <Popup>
-                  <h8>{site.properties.name}</h8>
-                 <p>{site.properties.amenity}</p>
-</Popup>
-              </Marker>
-            );
-         })}
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
-        </LeafletMap>
-      </React.Fragment>
-   //);
-  }
-}
-export default Maps;
-                                   <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
-                                </LeafletMap>
-                            </React.Fragment>
-    //);
-                          }
-                       
-                     export default Maps;
-      
-            );
-          })}
-        </LeafletMap></div>
+                <Marker position={[38.65, -90.25]}>
+                  <img
+                    src={
+                      "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
+                    }
+                    width="15"
+                  ></img>
+                </Marker>
+                {siteData.features.map(site => {
+                  console.log(site.geometry.coordinates);
+                  return (
+                    <Marker
+                      key={site.properties.amenity}
+                      position={[
+                        site.geometry.coordinates[1],
+                        site.geometry.coordinates[0]
+                      ]}
+                    >
+                      <Popup>
+                        <h8>{site.properties.name}</h8>
+                        <p>{site.properties.amenity}</p>
+                      </Popup>
+                    </Marker>
+                  );
+                })}
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
+              </LeafletMap>
+            </div>
 
             <div className="incent">
               <h6 id="staff">Staff Announcements</h6>
               <p>
                 <img src={schenk} alt="Schenk:" className="schenk" />
-                <img src={trophy} alt="" className="trophy" /> Schenk: Hawk House has
-                collected the most hours this semester and has won a free dress
-                down!
+                <img src={trophy} alt="" className="trophy" /> Schenk: Hawk
+                House has collected the most hours this semester and has won a
+                free dress down!
               </p>
             </div>
           </div>
--- src/pages/testMap/map.js
+++ src/pages/testMap/map.js
@@ -5,73 +5,52 @@ import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 //import "leaflet/dist/leaflet.css";
 import * as siteData from "./map.json";
 
- let config = {};
- config.params = {
-   center: [40.655769,-73.938503],
-  zoomControl: false,
-  zoom: 13,
-  maxZoom: 19,
-   minZoom: 11,
-  scrollwheel: false,
-  legends: true,
-  infoControl: false,
-  attributionControl: true
-};
-config.tileLayer = {
-  uri: 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
-  params: {
-   minZoom: 11,
-   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-   id: '',
-    accessToken: ''
-  }
- };
-
 class Maps extends Component {
   render() {
     return (
       <React.Fragment>
-       // <Navbar />
-        //<LeafletMap
+        // <Navbar />
+        //
+        <LeafletMap
           center={[38.65, -90.25]}
           zoom={11}
           attributionControl={true}
           zoomControl={true}
           doubleClickZoom={true}
-         scrollWheelZoom={false}
+          scrollWheelZoom={false}
           dragging={true}
           animate={true}
           easeLinearity={0.35}
         >
-         <Marker position={[38.65, -90.25]}>
+          <Marker position={[38.65, -90.25]}>
             <img
               src={
                 "https://upload.wikimedia.org/wikipedia/commons/8/88/Map_marker.svg"
               }
               width="15"
             ></img>
-</Marker>
+          </Marker>
           {siteData.features.map(site => {
             console.log(site.geometry.coordinates);
-           return (
+            return (
               <Marker
                 key={site.properties.amenity}
                 position={[
-                 site.geometry.coordinates[1],
+                  site.geometry.coordinates[1],
                   site.geometry.coordinates[0]
                 ]}
               >
                 <Popup>
                   <h8>{site.properties.name}</h8>
-                 <p>{site.properties.amenity}</p>
-</Popup>
+                  <p>{site.properties.amenity}</p>
+                </Popup>
               </Marker>
             );
-         })}
+          })}
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></TileLayer>
         </LeafletMap>
       </React.Fragment>
-   );
+    );
   }
 }
 export default Maps;
